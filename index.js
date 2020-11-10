e684ccb6f9ca2b70240b0f8ec0ab6d99ed36f7a6
Malo Lebrin <malolebrin@gmail.com>
2020-11-10 12:51:10 +0100

    add dotenv package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require('dotenv').config();
 app.use(cors());
 app.get("/", (req, res) => {
     console.log('entrée dans la requete')
